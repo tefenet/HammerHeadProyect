16e639ac12926d954913f495323473d72320b3d7
SantiLosa <santilosa22@gmail.com>
2018-05-25 22:41:15 -0300

    nombre apellido y fecha de nacimiento agregados a usuariosç

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :viajes
-  devise_for :users
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   root "application#index"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
