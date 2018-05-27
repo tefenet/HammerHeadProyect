01c0fbf8e6f782f702f1bae956b1c77e898fae67
SantiLosa <santilosa22@gmail.com>
2018-05-27 17:11:33 -0300

    mail de confirmacion de cuenta

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_005627) do
+ActiveRecord::Schema.define(version: 2018_05_26_172456) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 2018_05_26_005627) do
     t.string "first_name"
     t.string "last_name"
     t.string "birth_date"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
