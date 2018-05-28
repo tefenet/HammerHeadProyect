0ac13e558503ea42b6f6e6d623699fee442f8ecc
SantiLosa <santilosa22@gmail.com>
2018-05-28 20:29:25 -0300

    restricciones registro usuarios y mensajes de error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+	validates :birth_date, presence: { message: ": Por favor ingrese su fecha de nacimiento" }
+  validate :validate_age
+
+  validates :first_name, presence: { message: ": Por favor ingrese su nombre" }
+  validates :last_name, presence: { message: ": Por favor ingrese su apellido" }
+
+  validates :password, presence: { message: ": Por favor ingrese una contraseña" }
+
+  private
+
+  def validate_age
+      if birth_date.present? && birth_date.to_date > 18.years.ago.to_date
+          errors.add(:birth_date, 'Deberias ser mayor de 18 años.')
+      end
+  end
+
 end
