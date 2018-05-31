47bbf4ecae12f57a85c3b1d53d2be2056be524f9
SantiLosa <santilosa22@gmail.com>
2018-05-31 19:19:39 -0300

    cambiar contraseña estando logueado

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+	
   resources :viajes
+
   devise_for :users, :controllers => { registrations: 'registrations' }
+
   resources :users
+
   root "application#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
