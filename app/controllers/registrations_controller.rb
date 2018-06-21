fd04bee2ba489bc9329930c090b6e3940262fda0
SantiLosa <santilosa22@gmail.com>
2018-06-21 20:17:33 -0300

    mensajes de exito

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,5 +9,9 @@ class RegistrationsController < Devise::RegistrationsController
 	def account_update_params
 		params.require(:user).permit(:first_name, :last_name, :email, :birth_date, :password, :password_confirmation, :current_password, :credit_card_number)
 	end
+
+	def after_inactive_sign_up_path_for(resource)
+		new_user_session_path :action => :new, notice: "Por favor confirma tu cuenta para poder logearte!"
+	end
 	
 end
