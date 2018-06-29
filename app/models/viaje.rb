d4caad32baf8a9fe36b7b86ce99a32a1126c7b6d
LEONIDAS-0117 <facundo.vasco@gmail.com>
2018-06-29 04:12:40 -0300

    yolo

--- app/models/viaje.rb
+++ app/models/viaje.rb
@@ -9,6 +9,7 @@ class Viaje < ApplicationRecord
   validates :hora, presence: {message: ": Por favor ingrese una hora para el viaje"}, on: [:create, :new, :update]
   validates :precio, presence: { message: ": Por favor ingrese el precio del viaje"}, on: [:create, :new, :update]
   validates :duracion, presence: { message: ": Por favor ingrese una duracion para el viaje"}, on: [:create, :new, :update]
+  validate :validate_inicio
   validate :validate_fecha
 
   attr_accessor :car_id
@@ -22,19 +23,21 @@ class Viaje < ApplicationRecord
   end
 
   def validate_inicio
-    if 12.hour.ago.to_datetime fecha. hora.to_i  
+    initial_hour = DateTime.parse(fecha.to_s + ' ' + hora.to_s)
+    if fecha && hora && (initial_hour < 12.hour.from_now)
       errors.add(:inicio, ':El viaje no puede comenzar en menos de 12 horas')
     end
   end
 
   def validate_fecha
-    if fecha > 30.days.from_now
+    if fecha && (fecha > 30.days.from_now)
       errors.add(:inicio, ':La Fecha del viaje no puede ser mayor 30 dias desde hoy')
     end
   end
 
   def validate_usuario_no_tiene_viajes_en_ese_horario
-    if usuario.viaje.count>=1 && inicio.present? && fin.present?
+  initial_hour = DateTime.parse(fecha.to_s + ' ' + hora.to_s)
+    if inicio && fin && usuario.viaje
       #if usuario.viaje.where(auto_id: auto_id).where("inicio < ? and fin > ?"  ,(inicio+ 3.hours),(inicio+3.hours)).or(usuario.viaje.where(auto_id: auto_id).where("inicio < ? and fin > ?"  ,(fin+3.hours),(fin+3.hours))).or(usuario.viaje.where(auto_id: auto_id).where("inicio > ? and fin < ?"  ,(inicio+3.hours),(fin+3.hours))).count >= 1
       if usuario.viaje.where("inicio < ? and fin > ?"  ,(inicio+ 3.hours),(inicio+3.hours)).or(usuario.viaje.where("inicio < ? and fin > ?"  ,(fin+3.hours),(fin+3.hours))).or(usuario.viaje.where("inicio > ? and fin < ?"  ,(inicio+3.hours),(fin+3.hours))).count >= 1
         errors.add(:base, 'el usuario ya posee viaje en ese momento')
@@ -45,12 +48,12 @@ class Viaje < ApplicationRecord
   def validate_usuario_no_tiene_solicitudes_en_ese_horario
     if inicio.present? && fin.present?
       usuario.solicitud.where(aceptada: true).where(finalizado: false).or(usuario.solicitud.where(aceptada: false).where(rechazada: false)).each do |solicitud|
-        if (solicitud.viaje.inicio < (inicio + 3.hours) && solicitud.viaje.fin > (inicio + 3.hours)) || (solicitud.viaje.inicio < (fin + 3.hours) && solicitud.viaje.fin > (fin + 3.hours)) || (solicitud.viaje.inicio > (inicio + 3.hours) && solicitud.viaje.fin < (fin + 3.hours))
-          #usuario.solicitud.where("viaje.inicio < ? and viaje.fin > ?"  ,(viaje.inicio+ 3.hours),(viaje.inicio+3.hours)).or(usuario.solicitud.where("viaje.inicio < ? and viaje.fin > ?"  ,(viaje.fin+3.hours),(viaje.fin+3.hours))).or(usuario.solicitud.where("viaje.inicio > ? and viaje.fin < ?"  ,(viaje.inicio+3.hours),(viaje.fin+3.hours))).count >= 1
-          errors.add(:base, 'el usuario ya posee una solicitud en ese momento')
-        end
+      if (solicitud.viaje.inicio < (inicio + 3.hours) && solicitud.viaje.fin > (inicio + 3.hours)) || (solicitud.viaje.inicio < (fin + 3.hours) && solicitud.viaje.fin > (fin + 3.hours)) || (solicitud.viaje.inicio > (inicio + 3.hours) && solicitud.viaje.fin < (fin + 3.hours))
+        #usuario.solicitud.where("viaje.inicio < ? and viaje.fin > ?"  ,(viaje.inicio+ 3.hours),(viaje.inicio+3.hours)).or(usuario.solicitud.where("viaje.inicio < ? and viaje.fin > ?"  ,(viaje.fin+3.hours),(viaje.fin+3.hours))).or(usuario.solicitud.where("viaje.inicio > ? and viaje.fin < ?"  ,(viaje.inicio+3.hours),(viaje.fin+3.hours))).count >= 1
+        errors.add(:base, 'el usuario ya posee una solicitud en ese momento')
       end
     end
   end
+end
 
 end
