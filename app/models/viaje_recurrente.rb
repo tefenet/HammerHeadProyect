41c90c59a1a1226b09f0646da9fcfc82f59e286e
SantiLosa <santilosa22@gmail.com>
2018-08-05 20:16:08 -0300

    fix metodo date_of_next

--- app/models/viaje_recurrente.rb
+++ app/models/viaje_recurrente.rb
@@ -7,8 +7,9 @@ class ViajeRecurrente < ApplicationRecord
 	# https://stackoverflow.com/questions/7930370/ruby-code-to-get-the-date-of-next-monday-or-any-day-of-the-week
 
 	def date_of_next(day, semana)
+		semana = semana - 1
   		date  = Date.parse(day)
-  		delta = date > Date.today ? 0 : (7*semana)
+  		delta = date > (Date.today + (7*semana).days) ? 0 : 7
   		return date + delta
 	end
 end
